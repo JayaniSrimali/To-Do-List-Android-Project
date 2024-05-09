1979122e6f7e2dcf789a44f406b1bf17396baed7
JayaniSrimali <jayanisrimali666@gmail.com>
2024-05-09 16:47:39 +0530

    Project added to AddListActivity kotlin

--- app/src/main/java/com/example/to_dolistapp/AddListActivity.kt
+++ app/src/main/java/com/example/to_dolistapp/AddListActivity.kt
@@ -2,10 +2,28 @@ package com.example.to_dolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.to_dolistapp.databinding.ActivityAddListBinding
+import com.example.to_dolistapp.databinding.ActivityMainBinding
 
 class AddListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddListBinding
+    private lateinit var db:TODODatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_list)
+        binding = ActivityAddListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TODODatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val todo =  TODO(0,title,content)
+            db.insertTodo(todo)
+            Toast.makeText(this,"To to saved",Toast.LENGTH_SHORT).show()
+        }
     }
 }
