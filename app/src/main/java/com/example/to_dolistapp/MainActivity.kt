62a1f57b342b58f6ae8a8e4ba6d9d127b43d26b3
JayaniSrimali <jayanisrimali666@gmail.com>
2024-05-10 10:41:31 +0530

    Project added to MainActivity.kt kotlin

--- app/src/main/java/com/example/to_dolistapp/MainActivity.kt
+++ app/src/main/java/com/example/to_dolistapp/MainActivity.kt
@@ -3,17 +3,25 @@ package com.example.to_dolistapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.to_dolistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var binding: ActivityMainBinding
-
+    private lateinit var db : TODODatabaseHelper
+    private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = TODODatabaseHelper(this)
+        todoAdapter = TodoAdapter(db.getAllTodoList(),this)
+
+        binding.todoRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.todoRecyclerView.adapter = todoAdapter
 
         binding.addButton.setOnClickListener{
             val  intent =   Intent(this,AddListActivity::class.java)
@@ -21,5 +29,10 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        todoAdapter.refreshData(db.getAllTodoList())
     }
 }
